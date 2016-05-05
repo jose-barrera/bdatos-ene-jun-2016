d5d61c11fb9c047e3b48f7f7cb92e6cdc3c5ca61
PeterCP <pacp95@gmail.com>
2016-05-05 09:30:57 -0500

    Added Laravel Collective service provider for using dynamic forms.

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Laravel Collective Service Providers...
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +206,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Laravel Collective Aliases.
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
