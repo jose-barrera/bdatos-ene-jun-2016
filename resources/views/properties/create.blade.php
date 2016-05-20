5e9830ca45fb167c7cfdd67de39bd7f4ab46c804
PeterCP <pacp95@gmail.com>
2016-05-20 14:37:56 -0500

    Fixed formatting for properties forms.

--- resources/views/properties/create.blade.php
+++ resources/views/properties/create.blade.php
@@ -1,63 +1,65 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="col-lg-8 col-md-10 col-sm-12 col-lg-offset-2 col-md-offset-1">
-	<div class="panel panel-default">
-		<div class="panel-heading">Alta de Propiedades</div>
-		<div class="panel-body">
-			{{ Form::open(['route' => 'properties.store', 'method' => 'POST', 'class' => 'form-horizontal']) }}
-				{{ Form::hidden('lessor_id', $user->id )}}
+<div class="row">
+	<div class="col-lg-8 col-md-10 col-sm-12 col-lg-offset-2 col-md-offset-1">
+		<div class="panel panel-default">
+			<div class="panel-heading">Alta de Propiedades</div>
+			<div class="panel-body">
+				{{ Form::open(['route' => 'properties.store', 'method' => 'POST', 'class' => 'form-horizontal']) }}
+					{{ Form::hidden('lessor_id', $user->id )}}
 
-				<!-- Alias -->
-				<div class="form-group">
-					{{ Form::label('alias', 'Alias', ['class' => 'control-label col-md-4']) }}
-					<div class="col-md-6">
-						{{ Form::text('alias', null, ['class' => 'form-control']) }}
+					<!-- Alias -->
+					<div class="form-group">
+						{{ Form::label('alias', 'Alias', ['class' => 'control-label col-md-3']) }}
+						<div class="col-md-7">
+							{{ Form::text('alias', null, ['class' => 'form-control']) }}
+						</div>
 					</div>
-				</div>
 
-				<!-- Descripcion -->
-				<div class="form-group">
-					{{ Form::label('description', 'Descripción', ['class' => 'control-label col-md-4']) }}
-					<div class="col-md-6">
-						{{ Form::textarea('description', null, ['class' => 'form-control']) }}
+					<!-- Descripcion -->
+					<div class="form-group">
+						{{ Form::label('description', 'Descripción', ['class' => 'control-label col-md-3']) }}
+						<div class="col-md-7">
+							{{ Form::textarea('description', null, ['class' => 'form-control']) }}
+						</div>
 					</div>
-				</div>
 
-				<!-- Direccion -->
-				<div class="form-group">
-					{{ Form::label('address', 'Dirección', ['class' => 'control-label col-md-4']) }}
-					<div class="col-md-6">
-						{{ Form::textarea('address', null, ['class' => 'form-control']) }}
+					<!-- Direccion -->
+					<div class="form-group">
+						{{ Form::label('address', 'Dirección', ['class' => 'control-label col-md-3']) }}
+						<div class="col-md-7">
+							{{ Form::textarea('address', null, ['class' => 'form-control']) }}
+						</div>
 					</div>
-				</div>
 
-				<!-- Codigo postal -->
-				<div class="form-group">
-					{{ Form::label('postal_code', 'Código Postal', ['class' => 'control-label col-md-4']) }}
-					<div class="col-md-6">
-						{{ Form::number('postal_code', null, ['class' => 'form-control']) }}
+					<!-- Codigo postal -->
+					<div class="form-group">
+						{{ Form::label('postal_code', 'Código Postal', ['class' => 'control-label col-md-3']) }}
+						<div class="col-md-7">
+							{{ Form::number('postal_code', null, ['class' => 'form-control']) }}
+						</div>
 					</div>
-				</div>
 
-				<!-- Tipo de propiedad -->
-				<div class="form-group">
-					{{ Form::label('type_id', 'Tipo de propiedad', ['class' => 'control-label col-md-4']) }}
-					<div class="col-md-6">
-						{{ Form::select('type_id', $property_types->pluck('description', 'id'),
-							null, ['class' => 'form-control', 'placeholder' => 'Seleccionar...']) }}
+					<!-- Tipo de propiedad -->
+					<div class="form-group">
+						{{ Form::label('type_id', 'Tipo de propiedad', ['class' => 'control-label col-md-3']) }}
+						<div class="col-md-7">
+							{{ Form::select('type_id', $property_types->pluck('description', 'id'),
+								null, ['class' => 'form-control', 'placeholder' => 'Seleccionar...']) }}
+						</div>
 					</div>
-				</div>
 
-				<!-- Boton de submit -->
-				<div class="form-group">
-					<div class="col-md-6 col-md-offset-4">
-						<button type="submit" class="btn btn-primary">
-							<span class="glyphicon glyphicon-ok"></span> Guardar
-						</button>
+					<!-- Boton de submit -->
+					<div class="form-group">
+						<div class="col-md-7 col-md-offset-3">
+							<button type="submit" class="btn btn-primary btn-block">
+								<span class="glyphicon glyphicon-ok"></span> Guardar
+							</button>
+						</div>
 					</div>
-				</div>
-			{{ Form::close() }}
+				{{ Form::close() }}
+			</div>
 		</div>
 	</div>
 </div>
--- resources/views/properties/edit.blade.php
+++ resources/views/properties/edit.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-8 col-md-offset-2">
+    <div class="col-lg-8 col-md-10 col-sm-12 col-lg-offset-2 col-md-offset-1">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title">Editar propiedad: {{ $property->alias }}</h3>
+                <strong>Editar propiedad:</strong> {{ $property->alias }}
             </div>
             <div class="panel-body">
                 {{ Form::model($property, ['route' => ['properties.update', $property->id],
@@ -13,47 +13,51 @@
 
                     <!-- Alias -->
                     <div class="form-group">
-                        {{ Form::label('alias', 'Alias', ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
+                        {{ Form::label('alias', 'Alias', ['class' => 'col-md-3 control-label']) }}
+                        <div class="col-md-7">
                             {{ Form::text('alias', null, ['class' => 'form-control']) }}
                         </div>
                     </div>
 
                     <!-- Descripcion -->
                     <div class="form-group">
-                        {{ Form::label('description', 'Descripción', ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
+                        {{ Form::label('description', 'Descripción', ['class' => 'col-md-3 control-label']) }}
+                        <div class="col-md-7">
                             {{ Form::textarea('description', null, ['class' => 'form-control']) }}
                         </div>
                     </div>
 
                     <!-- Direccion -->
                     <div class="form-group">
-                        {{ Form::label('address', 'Dirección', ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
+                        {{ Form::label('address', 'Dirección', ['class' => 'col-md-3 control-label']) }}
+                        <div class="col-md-7">
                             {{ Form::text('address', null, ['class' => 'form-control']) }}
                         </div>
                     </div>
 
                     <!-- Codigo postal -->
                     <div class="form-group">
-                        {{ Form::label('postal_code', 'Código postal', ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
+                        {{ Form::label('postal_code', 'Código postal', ['class' => 'col-md-3 control-label']) }}
+                        <div class="col-md-7">
                             {{ Form::number('postal_code', null, ['class' => 'form-control']) }}
                         </div>
                     </div>
 
                     <!-- Tipo propiedad -->
                     <div class="form-group">
-                        {{ Form::label('type_id', 'Tipo de propiedad', ['class' => 'col-md-4 control-label']) }}
-                        <div class="col-md-6">
+                        {{ Form::label('type_id', 'Tipo de propiedad', ['class' => 'col-md-3 control-label']) }}
+                        <div class="col-md-7">
                             {{ Form::select('type_id', App\Models\PropertyType::pluck('description', 'id'),
                                 null, ['class' => 'form-control']) }}
                         </div>
                     </div>
 
-                    <div class="col-md-6 col-md-offset-4">
-                        {{ Form::submit('Guardar', ['class' => 'btn btn-primary']) }}
+                    <div class="form-group">
+                        <div class="col-md-7 col-md-offset-3">
+                            <button type="primary" class="btn btn-primary btn-block">
+                                <span class="glyphicon glyphicon-ok"></span> Guardar
+                            </button>
+                        </div>
                     </div>
                 {{ Form::close() }}
             </div>
